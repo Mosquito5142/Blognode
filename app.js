89aab03bc96c1511c7b18dde740e75cadd6fd5df
Vachiravit Doungdee <mos14.d@gmail.com>
2024-06-21 00:39:16 +0700

    postAddpost

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 const connection = require('./connection'); // Import the connection
 const postsRoutes = require('./routes/posts'); // Import posts routes
 const usersRoutes = require('./routes/users'); // Import users routes
+const bodyParser = require('body-parser')
 
 app.use(cors()); // Enable CORS for cross-origin requests
 
 // Use the routes
 app.use('/posts', postsRoutes);
 app.use('/users', usersRoutes);
+
 
 app.listen(3000, () => {
   console.log('CORS-enabled web server listening on port 3000');
--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const connectDB = require('../connection');
+const bodyParser = require('body-parser');
+
+// Use body-parser middleware in the router
+router.use(bodyParser.json());
 
 router.get('/', async (req, res) => {
   try {
     const connection = await connectDB();
-    const [postsResults] = await connection.query('SELECT `id`, `title`, `author`, `created_at`, `category` FROM `posts` WHERE 1');
+    const [postsResults] = await connection.query('SELECT `id`, `title`, `author`, `updated_at`, `category`,`image` FROM `posts` WHERE 1');
     res.json(postsResults);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
   }
 });
+
 router.get('/:id', async (req, res) => {
   const postId = req.params.id; // รับค่า id จาก URL parameter
 
@@ -24,6 +29,35 @@ router.get('/:id', async (req, res) => {
     }
 
     res.json(postResults[0]); // ส่งข้อมูลโพสท์ที่พบกลับไปในรูปแบบ JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/addposts', async (req, res) => {
+  const {
+    title,
+    content,
+    author,
+    created_at,
+    updated_at,
+    status,
+    category,
+    tags,
+    image,
+  } = req.body;
+
+  try {
+    const connection = await connectDB();
+    const query = `
+      INSERT INTO posts (title, content, author, created_at, updated_at, status, category, tags, image)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    const values = [title, content, author, created_at, updated_at, status, category, tags, image];
+    await connection.query(query, values);
+
+    res.status(201).json({ message: 'Post added successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
